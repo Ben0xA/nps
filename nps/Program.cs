11d439d2358cc808c1e57490b7221685b39a30ad
Ben0xA <web@ben0xa.com>
2015-12-18 12:53:25 -0600

    Initial Commit

---
+++ nps/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace nps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                if (args[0].ToLower() == "-encode")
+                {
+                    if(args.Length == 2)
+                    {
+                        Byte[] bytes = System.Text.Encoding.UTF8.GetBytes(args[1]);
+                        Console.WriteLine(System.Convert.ToBase64String(bytes));
+                    }
+                    else 
+                    {
+                        Console.WriteLine("usage: nps.exe -encode \"& commands; separated; by; semicolons;\"");
+                    }
+                }
+                else if (args[0].ToLower() == "-decode")
+                {
+                    if (args.Length == 2)
+                    {
+                        String cmd = System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(args[1]));
+                        Console.WriteLine(cmd);
+                    }
+                    else
+                    {
+                        Console.WriteLine("usage: nps.exe -decode {base_64_string}");
+                    }
+                }
+                else 
+                {
+                    PowerShell ps = PowerShell.Create();
+                    if (args[0].ToLower() == "-encodedcommand")
+                    {
+                        ps.AddScript(System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(args[1])));
+                    }
+                    else
+                    {
+                        ps.AddScript(args[0]);
+                    }                
+
+                    Collection<PSObject> output = ps.Invoke();
+                    if (output != null)
+                    {
+                        foreach (PSObject rtnItem in output)
+                        {
+                            Console.WriteLine(rtnItem.ToString());
+                        }
+                    }
+                }                
+            }
+            else
+            {
+                Console.WriteLine("usage:\r\nnps.exe \"{powershell single command}\"\r\nnps.exe \"& {commands; semi-colon; separated}\"\r\nnps.exe -encodedcommand {base64_encoded_command}\r\nnps.exe -encode \"commands to encode to base64\"\r\nnps.exe -decode {base64_encoded_command}");
+            }
+        }
+    }
+}
---
+++ nps/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Not PowerShell")]
+[assembly: AssemblyDescription("Written by: Ben Ten (ben0xa)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Not PowerShell")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("095f6025-0b19-4c8e-8221-c29d7ce8fc8c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
